b8891be176082585a192edbf0517a786827351cd
yokota9248 <yokotase20@gmail.com>
2025-01-10 19:26:46 +0900

    Delete import

--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,20 @@ type Job = {
   title: string;
   salary: number;
   category_name: string;
-};
-
-// サーバーサイドでデータを取得
-export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/jobs', {
-    cache: "no-store", // キャッシュを無効化
-  });
-  const data: Job[] = await res.json();
-
-  return {
-    props: {
-      jobs: data, // サーバーサイドで取得したデータをpropsとして渡す
-    },
-  };
 }
-
-type HomeProps = {
-  jobs: Job[];
-};
-
-export default function Home({ jobs }: HomeProps) {
+async function fetchJobs(): Promise<Job[]> {
+  const res = await fetch('http://localhost:3000/api/jobs', {
+    cache: "no-store",
+  });
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+export default async function Home() {
+  const jobs = await fetchJobs();
   return (
     <div className="flex">
-      <JobPage jobs={jobs} />
+      <JobPage jobs={jobs}/>
     </div>
   );
 }
